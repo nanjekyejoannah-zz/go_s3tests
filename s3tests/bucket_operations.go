ceacd05a498f996c560d2172452191333f6fa6d9
nanjekyejoannah <nanjekyejoannah@gmail.com>
2017-06-13 21:50:02 +0300

    Basic Bucket Operation Tests

---
+++ s3tests/bucket_operations.go
@@ -0,0 +1,287 @@
+package s3tests
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"bytes"
+	"fmt"
+	yaml "gopkg.in/yaml.v2"
+	"io"
+	"io/ioutil"
+	"strings"
+)
+
+type Configuration struct {
+	RGW          rgwConfig
+	JwtKey       string `yaml:"default_key"`
+	IgnoreString string `yaml:"-"`
+}
+
+type rgwConfig struct {
+	rgw_access_key_id     string `yaml:"key"`
+	rgw_secret_access_key string `yaml:"secret"`
+	bucket                string `yaml:"bucket"`
+	my_end_point          string `yaml:"endpoint"`
+	my_region             string `yaml:"region"`
+}
+
+func LoadConfig(path string) Configuration {
+	file, err := ioutil.ReadFile(path)
+	var config Configuration
+
+	err = yaml.Unmarshal(file, &config)
+	if err != nil {
+		fmt.Printf("Config Parse Error: ")
+	}
+
+	return config
+}
+
+var config = LoadConfig("../config.yaml")
+var creds = credentials.NewStaticCredentials(config.rgw_access_key_id, config.rgw_secret_access_key , "")
+
+var cfg = aws.NewConfig().WithRegion(config.my_region).
+	WithEndpoint(config.my_end_point).
+	WithDisableSSL(true).
+	WithLogLevel(3).
+	WithS3ForcePathStyle(true).
+	WithCredentials(creds)
+
+var sess = session.Must(session.NewSession())
+var svc = s3.New(sess, cfg)
+var uploader = s3manager.NewUploader(sess)
+var downloader = s3manager.NewDownloader(sess)
+
+func SliceContains(s []string, e string) bool {
+	resp := false
+
+	for _, a := range s {
+		if a == e {
+			resp = true
+			break
+		} else {
+			resp = false
+			break
+		}
+	}
+
+	return resp
+}
+
+func CreateBucket(bucket string) error {
+
+	_, err := svc.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(bucket),
+	})
+
+	err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
+		Bucket: aws.String(bucket),
+	})
+
+	return err
+}
+
+func CreateBucketAndKey(bucket string, key string) error {
+
+	_, err := svc.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	return err
+}
+
+func PutObjectToBucket(bucket string, key string, content string) error {
+
+	_, err := svc.PutObject(&s3.PutObjectInput{
+		Body:   strings.NewReader(content),
+		Bucket: &bucket,
+		Key:    &key,
+	})
+
+	return err
+}
+
+func DeleteBucket(bucket string) error {
+
+	_, err := svc.DeleteBucket(&s3.DeleteBucketInput{
+		Bucket: aws.String(bucket),
+	})
+
+	return err
+}
+
+func ListBuckets() ([]string, error) {
+
+	var bukts []string
+
+	result, err := svc.ListBuckets(nil)
+
+	for _, bucket := range result.Buckets {
+		bukts = append(bukts, aws.StringValue(bucket.Name))
+	}
+	return bukts, err
+}
+
+func ListObjects(bucket string) ([]*s3.Object, error) {
+
+	resp, err := svc.ListObjects(&s3.ListObjectsInput{
+		Bucket: aws.String(bucket),
+	})
+
+	return resp.Contents, err
+}
+
+func GetObject(bucket string, key string) (string, error) {
+
+	results, err := svc.GetObject(&s3.GetObjectInput{Bucket: aws.String(bucket), Key: aws.String(key)})
+
+	var resp string
+	var errr error
+
+	if err == nil {
+
+		buf := bytes.NewBuffer(nil)
+		if _, err := io.Copy(buf, results.Body); err != nil {
+			return "", err
+		}
+
+		byteArray := buf.Bytes()
+
+		resp, errr = string(byteArray[:]), err
+
+	} else {
+
+		resp, errr = "", err
+	}
+
+	return resp, errr
+}
+
+func DeleteObject(bucket string, key string) error {
+
+	_, err := svc.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String("Bucketname"),
+		Key:    aws.String("ObjectKey"),
+	})
+
+	return err
+}
+
+func DeleteObjects(bucket string) error {
+
+	resp, err := svc.ListObjects(&s3.ListObjectsInput{Bucket: aws.String(bucket)})
+
+	num_objs := len(resp.Contents)
+	var items s3.Delete
+	var objs = make([]*s3.ObjectIdentifier, num_objs)
+
+	for i, o := range resp.Contents {
+		objs[i] = &s3.ObjectIdentifier{Key: aws.String(*o.Key)}
+	}
+
+	items.SetObjects(objs)
+	_, err = svc.DeleteObjects(&s3.DeleteObjectsInput{Bucket: &bucket, Delete: &items})
+
+	return err
+}
+
+func GetKeys(bucket string) ([]string, error) {
+	var keys []string
+
+	resp, err := svc.ListObjects(&s3.ListObjectsInput{
+		Bucket: aws.String(bucket),
+	})
+
+	for _, key := range resp.Contents {
+		keys = append(keys, *key.Key)
+	}
+
+	return keys, err
+}
+
+func GetKeysWithMaxKeys(bucket string, maxkeys int64) ([]string, error) {
+	var keys []string
+
+	resp, err := svc.ListObjects(&s3.ListObjectsInput{
+		Bucket:  aws.String(bucket),
+		MaxKeys: aws.Int64(maxkeys),
+	})
+
+	for _, key := range resp.Contents {
+		keys = append(keys, *key.Key)
+	}
+
+	return keys, err
+}
+
+func GetKeysWithMaxKeysAndMarker(bucket string, maxkeys int64, marker string) ([]string, error) {
+
+	var keys []string
+
+	resp, err := svc.ListObjects(&s3.ListObjectsInput{
+		Bucket:  aws.String("bucket"),
+		MaxKeys: aws.Int64(maxkeys),
+		Marker:  aws.String(marker),
+	})
+
+	for _, key := range resp.Contents {
+		keys = append(keys, *key.Key)
+	}
+
+	return keys, err
+}
+
+func CopyObject(other string, source string, item string) error {
+
+	_, err := svc.CopyObject(&s3.CopyObjectInput{
+		Bucket:     aws.String(other),
+		CopySource: aws.String(source),
+		Key:        aws.String(item)})
+
+	return err
+}
+
+func SinglFileUpload(bucket string, filename string) error {
+
+	file, _ := os.Open(filename)
+	defer file.Close()
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(filename),
+		Body:   file,
+	})
+
+	return err
+}
+
+func SinglFileDownLoad(bucket string, item string) error {
+
+	file, err := os.Create(item)
+
+	defer file.Close()
+
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(item),
+		})
+
+	return err
+}
+
+func GeneratePresignedUrlGetObject(bucket string, key string) (string, error) {
+
+	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	urlStr, err := req.Presign(15 * time.Minute)
+
+	return urlStr, err
+}
---
+++ s3tests/s3BucketOperations_test.go
@@ -0,0 +1,444 @@
+package s3tests
+
+import (
+	assert "github.com/stretchr/testify/require"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+//.............Bucket Operations.............................................................
+
+func TestBucketCreateReadDelete(t *testing.T) {
+
+	//should be able to create, delete and list buckets.
+
+	assert := assert.New(t)
+	bucket := "bucket7"
+
+	err := CreateBucket(bucket)
+	assert.Nil(err)
+
+	bkts, err := ListBuckets()
+	assert.Nil(err)
+	assert.Equal(true, SliceContains(bkts, bucket))
+
+	err = DeleteBucket(bucket)
+	assert.Nil(err)
+}
+
+func TestBucketDeleteNonExistant(t *testing.T) {
+
+	// should not delete non existant bucket
+
+	assert := assert.New(t)
+	bucket := "bucket9"
+
+	err := DeleteBucket(bucket)
+	assert.NotNil(err)
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+
+			assert.Equal(awsErr.Code(), "NoSuchBucket")
+			assert.Equal(awsErr.Message(), "")
+			//assert.Equal(awsErr.Error().Code, 409)
+		}
+	}
+
+}
+
+func TestBucketDeleteNotEmpty(t *testing.T) {
+
+	// should not delete a bucket with contents
+
+	assert := assert.New(t)
+	bucket := "bucket1"
+
+	err := CreateBucket(bucket)
+	assert.Nil(err)
+
+	err = PutObjectToBucket(bucket, "key1", "Hello")
+
+	err = DeleteBucket(bucket)
+	assert.NotNil(err)
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+
+			assert.Equal(awsErr.Code(), "BucketNotEmpty")
+			assert.Equal(awsErr.Message(), "")
+			//assert.Equal(awsErr.Error().Code, 409)
+		}
+	}
+
+	err = DeleteObjects(bucket)
+	err = DeleteBucket(bucket)
+	assert.Nil(err)
+
+}
+
+func TestBucketCreateExists(t *testing.T) {
+
+	// should not create two buckets with the same name.
+
+	assert := assert.New(t)
+	bucket := "bucket8"
+
+	err := CreateBucket(bucket)
+	assert.Nil(err)
+
+	err = CreateBucket(bucket)
+	assert.Nil(err)
+
+	bkts, err := ListBuckets()
+	assert.Nil(err)
+	assert.Equal(1, len(bkts))
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+
+			assert.Equal(awsErr.Code(), "BucketAlreadyExists")
+			assert.Equal(awsErr.Message(), "Not Found")
+			//assert.Equal(awsErr.Error(), 409)
+
+		}
+	}
+
+	err = DeleteBucket(bucket)
+	assert.Nil(err)
+}
+
+func TestBucketListEmpty(t *testing.T) {
+
+	//empty bucket returns no contents
+
+	assert := assert.New(t)
+	bucket := "bucket1"
+	var empty_list []*s3.Object
+
+	err := CreateBucket(bucket)
+	assert.Nil(err)
+
+	objects, err := ListObjects(bucket)
+	assert.Nil(err)
+	assert.Equal(empty_list, objects)
+
+	err = DeleteBucket(bucket)
+	assert.Nil(err)
+}
+
+func TestBucketListDistinct(t *testing.T) {
+
+	// distinct buckets should have distinct contents
+
+	assert := assert.New(t)
+	bucket1 := "bucket1"
+	bucket2 := "bucket2"
+
+	err := CreateBucket(bucket1)
+	err = CreateBucket(bucket2)
+	assert.Nil(err)
+
+	err = PutObjectToBucket(bucket1, "key1", "Hello")
+	err = PutObjectToBucket(bucket2, "key2", "Manze")
+
+	obj1, _ := GetObject(bucket1, "key1")
+	obj2, _ := GetObject(bucket2, "key2")
+
+	assert.Equal(obj1, "Hello")
+	assert.Equal(obj2, "Manze")
+
+	err = DeleteObjects(bucket1)
+	err = DeleteObjects(bucket2)
+	err = DeleteBucket(bucket1)
+	err = DeleteBucket(bucket2)
+	assert.Nil(err)
+
+}
+
+func TestBucketListMany(t *testing.T) {
+
+	// pagination w/max_keys=2, no marker
+
+	assert := assert.New(t)
+	bucket := "bucket11"
+	expected_keys := []string{"key1", "key2"}
+	//expected_key := []string{"key1"}
+
+	err := CreateBucket(bucket)
+	assert.Nil(err)
+
+	err = PutObjectToBucket(bucket, "key1", "echo")
+	err = PutObjectToBucket(bucket, "key2", "lima")
+	err = PutObjectToBucket(bucket, "key3", "golf")
+	err = PutObjectToBucket(bucket, "key4", "alpha")
+	assert.Nil(err)
+
+	var keys []string
+	keys, err = GetKeys(bucket)
+	assert.Nil(err)
+	assert.Equal(4, len(keys))
+
+	keys, err = GetKeysWithMaxKeys(bucket, 2)
+	assert.Nil(err)
+	assert.Equal(2, len(keys))
+	assert.Equal(expected_keys, keys)
+
+	// keys, err = GetKeysWithMaxKeysAndMarker(bucket, 2, expected_keys[0] )
+	// assert.Nil(err)
+	// assert.Equal(1, len(keys))
+	// assert.Equal(expected_key, keys)
+
+	err = DeleteObjects(bucket)
+	err = DeleteBucket(bucket)
+	assert.Nil(err)
+
+}
+
+//................................................Object Operations...............................................................................
+
+func TestObjectReadNotExist(t *testing.T) {
+
+	// Reading content that was never written should fail
+
+	assert := assert.New(t)
+	bucket1 := "bucket1"
+
+	err := CreateBucket(bucket1)
+	assert.Nil(err)
+
+	_, err = GetObject(bucket1, "key6")
+	assert.NotNil(err)
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+
+			assert.Equal(awsErr.Code(), "NoSuchKey")
+			assert.Equal(awsErr.Message(), "")
+			//assert.Equal(awsErr.Error(), 404)
+
+		}
+	}
+
+	err = DeleteBucket(bucket1)
+	assert.Nil(err)
+
+}
+
+func TestObjectWriteToNonExistBucket(t *testing.T) {
+
+	// writing to a non existant bucket should fail
+
+	assert := assert.New(t)
+	non_exixtant_bucket := "bucketz"
+
+	_, err := GetObject(non_exixtant_bucket, "key6")
+	assert.NotNil(err)
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+
+			assert.Equal(awsErr.Code(), "NoSuchBucket")
+			assert.Equal(awsErr.Message(), "")
+		}
+
+	}
+
+}
+
+func TestObjectWriteReadUpdateReadDelete(t *testing.T) {
+
+	// Reading content that was never written should fail
+	assert := assert.New(t)
+	bucket := "bucket5"
+	key := "key5"
+
+	err := CreateBucket(bucket)
+	assert.Nil(err)
+
+	// Write object
+	err = PutObjectToBucket(bucket, key, "hello")
+	assert.Nil(err)
+
+	// Read object
+	result, _ := GetObject(bucket, key)
+	assert.Equal(result, "hello")
+
+	//Update object
+	err = PutObjectToBucket(bucket, key, "Come on !!")
+	assert.Nil(err)
+
+	// Read object again
+	result, _ = GetObject(bucket, key)
+	assert.Equal(result, "Come on !!")
+
+	err = DeleteObject(bucket, key)
+	assert.Nil(err)
+
+	// If object was well deleted, there shouldn't be an error at this point
+	err = DeleteBucket(bucket)
+	assert.Nil(err)
+}
+
+func TestObjectDeleteMany(t *testing.T) {
+
+	// Reading content that was never written should fail
+	assert := assert.New(t)
+
+	var empty_list []*s3.Object
+	bucket := "bucket5"
+	key := "key5"
+	key1 := "key55"
+
+	err := CreateBucket(bucket)
+	assert.Nil(err)
+
+	err = PutObjectToBucket(bucket, key, "hello")
+	err = PutObjectToBucket(bucket, key1, "foo")
+	assert.Nil(err)
+	assert.Equal(2, len(objects))
+
+	err = DeleteObjects(bucket)
+	assert.Nil(err)
+
+	objects, err := ListObjects(bucket)
+	assert.Nil(err)
+	assert.Equal(empty_list, objects)
+
+	err = DeleteBucket(bucket)
+	assert.Nil(err)
+}
+
+func TestObjectCopyBucketNotFound(t *testing.T) {
+
+	// copy from non-existent bucket
+
+	assert := assert.New(t)
+	bucket := "bucket4"
+	item := "key1"
+	other := "bucket2"
+
+	source := bucket + "/" + item
+
+	err := CreateBucket(bucket)
+	assert.Nil(err)
+
+	// Write object
+	err = PutObjectToBucket(bucket, item, "hello")
+	assert.Nil(err)
+
+	err = CopyObject(other, source, item)
+	assert.NotNil(err)
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+
+			assert.Equal(awsErr.Code(), "NoSuchBucket")
+			assert.Equal(awsErr.Message(), "")
+		}
+
+	}
+
+	err = DeleteObjects(bucket)
+	assert.Nil(err)
+
+	err = DeleteBucket(bucket)
+	assert.Nil(err)
+
+}
+
+func TestObjectCopyKeyNotFound(t *testing.T) {
+
+
+	assert := assert.New(t)
+	bucket := "bucket4"
+	item := "key1"
+	other := "bucket2"
+
+	source := bucket + "/" + item
+
+	err := CreateBucket(bucket)
+	err = CreateBucket(other)
+	assert.Nil(err)
+
+	err = CopyObject(other, source, item)
+	assert.NotNil(err)
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+
+			assert.Equal(awsErr.Code(), "NoSuchKey")
+			assert.Equal(awsErr.Message(), "")
+		}
+
+	}
+
+	err = DeleteObjects(bucket)
+	assert.Nil(err)
+
+	err = DeleteBucket(bucket)
+	assert.Nil(err)
+
+}
+
+func TestObjectCopyToDiffBucket(t *testing.T) {
+
+
+	assert := assert.New(t)
+	bucket := "bucket4"
+	item := "key1"
+	other := "bucket2"
+
+	source := bucket + "/" + item
+
+	err := CreateBucket(bucket)
+	err = CreateBucket(source)
+	assert.Nil(err)
+
+	// Write object
+	err = PutObjectToBucket(source, item, "hello")
+	assert.Nil(err)
+
+	err = CopyObject(other, source, item)
+	assert.NotNil(err)
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+
+			assert.Equal(awsErr.Code(), "NoSuchBucket")
+			assert.Equal(awsErr.Message(), "")
+		}
+
+	}
+
+	err = DeleteObjects(bucket)
+	err = DeleteObjects(source)
+	assert.Nil(err)
+
+	err = DeleteBucket(bucket)
+	err = DeleteBucket(source)
+	assert.Nil(err)
+
+}
+
+func TestSingleFileUploadAndDownload(t *testing.T) {
+
+	assert := assert.New(t)
+	bucket := "bucketz"
+	filename := "sample_file.txt"
+
+	err := SinglFileUpload(bucket, filename)
+	assert.Nil(err)
+
+	err = SinglFileDownLoad(bucket, filename)
+	assert.Nil(err)
+
+	err = DeleteObjects(bucket)
+	assert.Nil(err)
+
+	err = DeleteBucket(bucket)
+	assert.Nil(err)
+
+}
