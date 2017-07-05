8864558474e915bc861fec180b5c0d37c2d125ba
nanjekyejoannah <nanjekyejoannah@gmail.com>
2017-07-06 00:23:18 +0300

    host style

--- s3tests/hoststyle_test.go
+++ s3tests/hoststyle_test.go
@@ -6,8 +6,7 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
+	//"github.com/aws/aws-sdk-go/aws"
 	//"github.com/aws/aws-sdk-go/awstesting/unit"
 	"github.com/spf13/viper"
 )
@@ -30,53 +29,38 @@ func getUrl(bktname string)(string){
 	return url
 }
 
-// func (suite *HostStyleSuite) TestAccelerateNoSSLBucketBuild() {
-
-// 	assert := suite
-
-// 	tests := []s3BucketTest{
-// 		{"a.b.c", getUrl("a.b.c"), ""},
-// 		{"a..bc", getUrl("a..bc"), ""},
-// 	}
-
-// 	for _, test := range tests {
-// 		req, _ := svc.ListObjectsRequest(&s3.ListObjectsInput{Bucket: &test.bucket})
-// 		req.Build()
-// 		assert.Equal (test.url, req.HTTPRequest.URL.String())
-// 	}
-// }
-
-func (suite *HostStyleSuite) TestHostStyleBucketBuildNoSSL() {
+func (suite *HostStyleSuite) TestAccelerateNoSSLBucketBuild() {
 
 	assert := suite
 
 	tests := []s3BucketTest{
-		{"abc", getUrl("abc/key"), ""},
-		// {"a.b.c", getUrl("a.b.c"), ""},
-		// {"a$b$c", getUrl("%7BBucket%7D"), "InvalidParameterException"},
+		{"a.b.c", getUrl("a.b.c"), ""},
+		{"a..bc", getUrl("a..bc"), ""},
 	}
 
 	for _, test := range tests {
+		req, _ := svc.ListObjectsRequest(&s3.ListObjectsInput{Bucket: &test.bucket})
+		req.Build()
+		assert.Equal (test.url, req.HTTPRequest.URL.String())
+	}
+}
 
-		input := &s3.CopyObjectInput{
-			Bucket:         aws.String("bucket"),
-			CopySource:     aws.String("bucket/source"),
-			Key:            aws.String("key"),
-		}
+func (suite *HostStyleSuite) TestHostStyleBucketBuildNoSSL() {
 
-		input.SetWebsiteRedirectLocation(string(test.url))
-		req, _ := svc.CopyObjectRequest(input)
-		err := req.Send()
+	//svc := s3.New(unit.Session, &aws.Config{DisableSSL: aws.Bool(true)})
 
-		assert.Equal(test.url, req.HTTPRequest.URL.String())
+	assert := suite
 
-		if err != nil {
-			if awsErr, ok := err.(awserr.Error); ok {
+	tests := []s3BucketTest{
+		{"abc", getUrl("abc"), ""},
+		{"a.b.c", getUrl("a.b.c"), ""},
+		{"a$b$c", getUrl("a%24b%24c"), "InvalidParameterException"},
+	}
 
-				assert.Equal(awsErr.Code(), test.errCode)
-				assert.Equal(awsErr.Message(), "")
-			}
-		}
+	for _, test := range tests {
+		req, _ := svc.ListObjectsRequest(&s3.ListObjectsInput{Bucket: &test.bucket})
+		req.Build()
+		assert.Equal (test.url, req.HTTPRequest.URL.String())
 	}
 }
 
