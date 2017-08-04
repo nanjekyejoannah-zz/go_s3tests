3b06816858e1c634b330e4245d90f51b5ed97107
Joannah Nanjekye <nanjekyejoannah@yahoo.com>
2017-08-04 12:04:00 +0300

    Removed dead code

--- s3tests/awsv4_test.go
+++ s3tests/awsv4_test.go
@@ -1,8 +1,7 @@
 package s3test
 
 import (
-	//"github.com/stretchr/testify/suite"
-
+	
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/signer/v4"
 	"github.com/aws/aws-sdk-go/service/s3"
